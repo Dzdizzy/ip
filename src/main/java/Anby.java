286f3b0f9b973be107a1bd28d1031e4ef6895cd9
Dzdizzy <173619399+Dzdizzy@users.noreply.github.com>
2026-08-20 19:04:31 +0800

    added echo feature

--- src/main/java/Anby.java
+++ src/main/java/Anby.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Anby {
     public static void main(String[] args) {
         String banner = "    ___          __         \n"
@@ -8,9 +10,26 @@ public class Anby {
                 + "                   /____/\n";
 
         String line = "____________________________________________________________\n";
-        String greet = "Hey, I'm Anby\n"
-                + "What do you need me for? I accept payment only in burgers\n"
-                + "Alright see you. Don't forget my burgers okay\n";
-        System.out.println(banner + line + greet + line);
+
+        String intro = "Hey, I'm Anby\n"
+                + "What do you need me for? I accept payment only in burgers\n";
+
+        String end = "Alright see you. Don't forget my burgers okay";
+        String greet = banner + line + intro;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(greet);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("Bye")){
+                System.out.println(end);
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
