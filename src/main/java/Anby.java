85d3c510510cab6bbd976d97f6cabd98326f67bc
Dzdizzy <173619399+Dzdizzy@users.noreply.github.com>
2026-08-20 18:04:30 +0800

    renamed to anby, added greeting

---
+++ src/main/java/Anby.java
@@ -0,0 +1,16 @@
+public class Anby {
+    public static void main(String[] args) {
+        String banner = "    ___          __         \n"
+                + "   /   |  ____  / /_  __  __\n"
+                + "  / /| | / __ \\/ __ \\/ / / /\n"
+                + " / ___ |/ / / / /_/ / /_/ / \n"
+                + "/_/  |_/_/ /_/_.___/\\__, /  \n"
+                + "                   /____/\n";
+
+        String line = "____________________________________________________________\n";
+        String greet = "Hey, I'm Anby\n"
+                + "What do you need me for? I accept payment only in burgers\n"
+                + "Alright see you. Don't forget my burgers okay\n";
+        System.out.println(banner + line + greet + line);
+    }
+}
